67ad8afbf2e052b49f432732f3ba51214439428d
modasser-nayem <modassernayem@gmail.com>
2026-07-13 14:33:04 +0600

    Fix pagination helper default sortBy field

--- src/helpers/pagination.ts
+++ src/helpers/pagination.ts
@@ -18,7 +18,7 @@ const calculatePagination = (options: IPaginationOptions): IOptionsResult => {
   const limit: number = Number(options.limit) || 10;
   const skip: number = (Number(page) - 1) * limit;
 
-  const sortBy: string = options.sortBy || "createdAt";
+  const sortBy: string = options.sortBy || "created_at";
   const sortOrder: string = options.sortOrder || "desc";
 
   return {
